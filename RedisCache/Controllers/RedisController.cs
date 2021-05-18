ca1c00adff799dc3e3236c30950ff95d8396bb33
mtyuksel <yukselmtevfik@gmail.com>
2021-05-18 19:42:57 +0300

    Add project files.

---
+++ RedisCache/Controllers/RedisController.cs
@@ -0,0 +1,38 @@
+﻿using EasyCaching.Core;
+using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace RedisCache.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RedisController : ControllerBase
+    {
+        private IEasyCachingProvider _provider;
+        private IEasyCachingProviderFactory _providerFactory;
+
+        public RedisController(IEasyCachingProviderFactory providerFactory)
+        {
+            this._providerFactory = providerFactory;
+            this._provider = this._providerFactory.GetCachingProvider("myredis");
+        }
+
+
+        [HttpGet("set")]
+        public IActionResult Set(MyClass myClass)
+        {
+            myClass.Date = DateTime.Now;
+            this._provider.Set("MyKey", myClass, TimeSpan.FromMinutes(30));
+
+            return Ok();
+        }
+
+        [HttpGet("get")]
+        public IActionResult Get()
+        {
+            var result = this._provider.Get<MyClass>("MyKey");
+
+            return Ok(result);
+        }
+    }
+}
---
+++ RedisCache/MyClass.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace RedisCache
+{
+    [Serializable]
+    public class MyClass
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public Decimal Price { get; set; }
+        public bool Status { get; set; }
+    }
+}
